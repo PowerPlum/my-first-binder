d3c97a963889d61a6d9ceb762345f949b62ed126
Chris Bridge <chris@bridgeboys.co.uk>
2020-08-06 07:01:23 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
+
+# tutorial for jupyter and binder
+# copied from:
+# https://github.com/alan-turing-institute/the-turing-way/blob/master/workshops/boost-research-reproducibility-binder/workshop-presentations/zero-to-binder-python.md#whats-happening-in-the-background---part-1
+
 print("hello world")
 
 import numpy as np
